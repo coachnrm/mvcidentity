745176c2070e4844f2de661a55280c2ff5ef936c
Coach <nuttapong_coach@hotmail.com>
2024-07-19 15:27:33 +0700

    add dockerfile

--- obj/Debug/net8.0/Yt_Dot6Identity.AssemblyInfo.cs
+++ obj/Debug/net8.0/Yt_Dot6Identity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Yt_Dot6Identity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cedce4e20f7db23db487eeaeb9491468f214d3bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a3db1428ebef765865226ecd611ff1a0af06f72")]
 [assembly: System.Reflection.AssemblyProductAttribute("Yt_Dot6Identity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Yt_Dot6Identity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
